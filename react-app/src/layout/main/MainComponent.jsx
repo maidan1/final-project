5e76360f1c534142e282fb15e4a1c06c270e37d7
maidan1 <136680901+maidan1@users.noreply.github.com>
2024-03-09 18:51:53 +0200

    Update MainComponent.jsx

--- react-app/src/layout/main/MainComponent.jsx
+++ react-app/src/layout/main/MainComponent.jsx
@@ -2,7 +2,7 @@ import { Container } from "@mui/material";
 
 const MainComponent = ({ children }) => {
   return (
-    <Container sx={{ mb: 15, mt: 20, minHeight: "65vh", marginBottom: "15%" }}>
+    <Container sx={{ mt: 20, minHeight: "65vh", marginBottom: "7%" }}>
       {children}
     </Container>
   );
